e58ec8578b1430559be8203b5ca606ef56dbda8d
Tolben <vitek.pasek@gmail.com>
2025-08-09 17:43:31 +0200

    icon absolute path...

--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="sk">
 <head>
-  <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
-  <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Ponorte sa do sveta PDF kníh priamo od slovenských autorov.">
